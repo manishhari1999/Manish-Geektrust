7cb01384942fcf05f4f5bfbc4a76d538c26bec85
PURUSHOTHAM REDDY GANGOLLA <manishhari1999@gmail.com>
2023-09-22 15:35:25 +0530

    GeekTrust

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainPage from './components/main/MainPage';
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
